43e591f6695a02f69659c8613ed7dcc66a13fc52
student <aleewolfram@gmail.com>
2020-04-17 16:59:44 -0700

    loadtest updates for user & msg

--- tests/loadTest.py
+++ tests/loadTest.py
@@ -17,34 +17,31 @@ description = fake.sentence(nb_words=3, variable_nb_words=True, ext_word_list=No
 username = fake.user_name()
 url = fake.image_url()
 community_name = fake.word(ext_word_list=community_list)
+postID = fake.random_int(min=1, max=200, step=1)
+postAmount = fake.random_int(min=1, max=200, step=1)
 
 # generate bogus data for voting microservice
-voteID = fake.random_int(min=400, max=500, step=1)
+voteID = fake.random_int(min=1, max=200, step=1)
 voteID_json = {"vote_id":voteID}
-
-postID = fake.random_int(min=400, max=500, step=1)
-list_vote_ID = {"post_ids":[postID, postID]}
-
-postAmount = fake.random_int(min=1, max=200, step=1)
 
 # generate bogus data for msg microservice
 user_from = fake.user_name()
 user_to = fake.user_name()
 msg_content = fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None)
 msg_flag = fake.sentence(nb_words=3, variable_nb_words=True, ext_word_list=None)
-
 msgID = fake.random_int(min=400, max=500, step=1)
 
 # generate bogus data for user microservice
 username = fake.user_name()
 email = fake.email()
-user_json = {"username":username, "email":email}
-username_json = {"username":username}
 
 # json data
 dataPost = {"title":title,"description":description,"username":username,"community_name":community_name,"resource_url":url}
 myheaders = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-dataMsg = {"user_from":uder_from, "user_to":user_to, "msg_content":msg_content, "msg_flag":msg_flag}
+dataMsg = {"user_from":user_from, "user_to":user_to, "msg_content":msg_content, "msg_flag":msg_flag}
+list_vote_ID = {"post_ids":[postID, postID]}
+user_json = {"username":username, "email":email}
+username_json = {"username":username}
 
 class UserBehaviour(TaskSet):
 
@@ -56,10 +53,6 @@ class UserBehaviour(TaskSet):
     @task(2)
     def getPost(self):
         self.client.get("/posts/get?post_id=%d"%(postID))
-
-    # @task(5)
-    # def deletePost(self):
-    #     self.client.delete("/delete?post_id=%d"%(postID))
 
     @task(2)
     def getNPostbyCommunity(self):
@@ -91,34 +84,30 @@ class UserBehaviour(TaskSet):
         self.client.post("/votes/getList", json= list_vote_ID, headers= myheaders)
 
     # Messaging
-
     @task(5)
     def sendMessage(self):
         self.client.post("/messages/send", json= dataMsg, headers= myheaders)
 
-    @task(6)
+    @task(5)
     def favoriteMessage(self):
         self.client.get("/messages/favorite?msg_id=%d"%(msgID))
 
     # User
-
-    @task(7)
+    @task(6)
     def registerUser(self):
         self.client.post("/users/register", json=user_json, headers = myheaders)
 
-    @task(8)
+    @task(7)
     def updateUserEmail(self):
         self.client.post("/users/update_email", json=user_json, headers = myheaders)
 
-    @task(7)
+    @task(6)
     def addKarma(self):
         self.client.post("/users/add_karma", json=username_json, headers = myheaders)
 
-    @task(7)
+    @task(6)
     def removeKarma(self):
         self.client.post("/users/remove_karma", json=username_json, headers = myheaders)
-
-
 
 class WebsiteUser(HttpLocust):
 
